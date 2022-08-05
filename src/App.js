f6fa73c7df1ec07eb13aae584dd073c5575a68a3
MarisolHO <101591119+MarisolHO@users.noreply.github.com>
2022-08-05 00:12:31 -0500

    Added a Shecodes logo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import shecodeslogo from "./shecodeslogo.png";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src={shecodeslogo}
+          className="App-logo img-fluid"
+          alt="shecodeslogo"
+        />
+      </header>
+    </div>
+  );
+}
