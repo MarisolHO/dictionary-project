8ae0df53854ea6395eded328ac605112dba2c81f
MarisolHO <101591119+MarisolHO@users.noreply.github.com>
2022-08-05 13:27:52 -0500

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import shecodeslogo from "./shecodeslogo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img
-          src={shecodeslogo}
-          className="App-logo img-fluid"
-          alt="shecodeslogo"
-        />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img
+            src={shecodeslogo}
+            className="App-logo img-fluid"
+            alt="shecodeslogo"
+          />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Marisol HO</small>
+        </footer>
+      </div>
     </div>
   );
 }
