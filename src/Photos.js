43ab13d252be8783acfdc0614f2e46b8fe717883
MarisolHO <101591119+MarisolHO@users.noreply.github.com>
2022-08-12 12:41:33 -0500

    Fixed alt

--- src/Photos.js
+++ src/Photos.js
@@ -14,7 +14,11 @@ export default function Photos(props) {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <img src={photo.src.landscape} className="img-fluid" />
+                  <img
+                    src={photo.src.landscape}
+                    alt={`photo ${index}`}
+                    className="img-fluid"
+                  />
                 </a>
               </div>
             );
